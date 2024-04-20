43f9b67905fb1acefbf01dd47e00b219b79ae24c
bravebuttercup <batorbogi@gmail.com>
2024-04-20 15:34:24 +0200

    Javascript search bar function

--- main.js
+++ main.js
@@ -1,49 +1,33 @@
-// containerbe megjeleniteni az elemeket
 
 const CONTAINER = document.querySelector(".js-container");
 const API_URL = 'https://dummyjson.com/recipes?limit=100'
+const INPUT = document.querySelector(".js-input")
+const FORM = document.querySelector(".js-form")
 
+const RECIPES = await fetchData(API_URL);
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.recipes;
+}
+
+
+/*
 fetch(API_URL)
   .then(response => response.json())
   .then(data => render(data.recipes));
+*/
+/*First*/
+function render(data) {
+  const recipes = data.map(recipe => createRecipeTemplate(recipe)).join('');
+  console.log(recipes)
 
+  CONTAINER.innerHTML = recipes;
 
-
-
-/*Harmadik függvény*/function createStar(recipeRating) {
-  let rating = Math.floor(recipeRating);
-  let rest = Math.round((recipeRating - rating) * 10);
-  let starsHTML = ``;
-
-  // half <i class="fa-solid fa-star-half-stroke"></i>
-
-  for (let i = 0; i < 5; i++) {
-
-    if (rating > 0) {
-      starsHTML += `<i class="fa-solid fa-star"></i>`;
-      rating -= 1;
-    } else if (rest > 0) {
-      if (rest < 4) {
-        starsHTML += `<i class="fa-regular fa-star"></i>`
-      } else if (rest < 7) {
-        starsHTML += `<i class="fa-solid fa-star-half-stroke"></i>`;
-
-      } else {
-        starsHTML += `<i class="fa-solid fa-star"></i>`;
-      }
-      rest = 0;
-    } else {
-      starsHTML += `<i class="fa-regular fa-star"></i>`;
-    }
-  }
-
-  return starsHTML;
 }
-
-/* Negyedikk függvény */function createParagraph(list) {
-  return list.map((item) => `<p>${item}</p>`).join("");
-}
-/*Második függvény */function createRecipeTemplate(recipe) {
+/* Second */
+function createRecipeTemplate(recipe) {
   console.log(recipe)
   const starsHTML = createStar(recipe.rating);
   const ingredients = createParagraph(recipe.ingredients);
@@ -83,10 +67,53 @@ fetch(API_URL)
     </div>
   </div>`
 }
-/*Az első függvény */function render(data) {
-  const recipes = data.map(recipe => createRecipeTemplate(recipe)).join('');
-  console.log(recipes)
+/* Third */
+function createStar(recipeRating) {
+  let rating = Math.floor(recipeRating);
+  let rest = Math.round((recipeRating - rating) * 10);
+  let starsHTML = ``;
 
-  CONTAINER.innerHTML = recipes;
+  // half <i class="fa-solid fa-star-half-stroke"></i>
 
+  for (let i = 0; i < 5; i++) {
+
+    if (rating > 0) {
+      starsHTML += `<i class="fa-solid fa-star"></i>`;
+      rating -= 1;
+    } else if (rest > 0) {
+      if (rest < 4) {
+        starsHTML += `<i class="fa-regular fa-star"></i>`
+      } else if (rest < 7) {
+        starsHTML += `<i class="fa-solid fa-star-half-stroke"></i>`;
+
+      } else {
+        starsHTML += `<i class="fa-solid fa-star"></i>`;
+      }
+      rest = 0;
+    } else {
+      starsHTML += `<i class="fa-regular fa-star"></i>`;
+    }
+  }
+
+  return starsHTML;
 }
+/* Fourth */
+function createParagraph(list) {
+  return list.map((item) => `<p>${item}</p>`).join("");
+}
+
+/* Fifth */
+
+function inputEventHandler(e) {
+  const filterValue = e.target.value.toLowerCase();
+  const filteredData = RECIPES.filter((recipe) => {
+    return (
+      recipe.name.toLowerCase().includes(filterValue) || recipe.ingredients.join('').toLowerCase().includes(filterValue)
+    );
+  });
+  render(filteredData);
+}
+
+INPUT.addEventListener('input', inputEventHandler)
+FORM.addEventListener('submit', e => e.preventDefault())
+render(RECIPES);
